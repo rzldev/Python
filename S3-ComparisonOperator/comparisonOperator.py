6c4043abfce322ddb2adcb9fa91f34715c0e53f5
rzldev <49812100+rzldev@users.noreply.github.com>
2020-03-25 07:09:07 +0700

    Python Comparison Operator

---
+++ S3-ComparisonOperator/comparisonOperator.py
@@ -0,0 +1,60 @@
+## Introduction to comparison operator
+print(3 == 3)
+
+print(3 == 2)
+
+print('hello' == 'bye')
+
+print('Bye' == 'bye')
+
+print('2' == 2)
+
+print(2.0 == 2)
+
+print(3 != 2)
+
+print(3 != 3)
+
+print(3 > 2)
+
+print(2 > 3)
+
+print(5 < 7)
+
+print(7 < 5)
+
+print(2 <= 2)
+
+print(8 >= 12)
+
+print()
+
+## Comparison operator with logical operators
+print(1 < 2 < 3)
+
+print(1 < 2 > 3)
+
+print()
+
+# and
+print(1 < 2 and 2 < 3)
+
+print(1 < 2 and 2 > 3)
+
+print(1 == 2 and 2 == 3)
+
+print()
+
+# or
+print('good' == 'good' or 1 == 1)
+
+print(100 == 200 or 'bye' == 'bye')
+
+print(123 > 456 or 3 <= 1)
+
+print()
+
+# not
+print(not 100 == 100)
+
+print(not 400 > 9000)
